62bf3d823b8cf482254be67850da88d4c1d7f252
Dixon Pedraza <jdpedraza@vectoritcgroup.com>
2019-12-11 10:52:25 -0500

    avances

--- install.sh
+++ install.sh
@@ -1,11 +1,66 @@
 #!/bin/bash
 
-# variables
-
+# VARIABLES 
+ROOTUSER="root"
 DIR_SCRITP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ROOTUSER=root
-KEYPASS=123456
+printf "\n"
+printf "Ingrese el hostname ej. octopus.vectoritcgroup.com\n"
+printf "Si no existe ingrese la ip publica\n"
+printf "Si no existe ingrese la ip local\n"
+printf "[domain or ip]: "
+read -r HOST_MACHINE
+if [ -z "$HOST_MACHINE" ]
+then
+    echo "No valido"
+    exit 1
+fi
+
+printf "\n"
+printf "ingrese su ip publica ej. 181.181.181.181\n"
+printf "Si no existe ingrese la ip local\n"
+printf "[ip]: "
+read IP_MACHINE
+if [ -z "$IP_MACHINE" ]
+then
+    echo "No valido"
+    exit 1
+fi
+
+
+printf "\n"
+printf "ingrese nombre de la compania. ej. Vector ITC Group\n"
+printf "Si no existe ingrese la ip local\n"
+printf "[compania]: "
+read COMPANY
+if [ -z "$COMPANY" ]
+then
+    echo "No valido"
+    exit 1
+fi
+
+printf "\n"
+printf "ingrese contasena para el usuario root en la base de datos.\n"
+printf "Minimo 6 caracteres\n"
+printf "[mysql pass]: "
+read KEYPASS
+if [ -z "$KEYPASS" ]
+then
+    echo "No valido"
+    exit 1
+fi
+
+printf "\n"
+printf "ingrese contasena para el usuario de asterisk en la base de datos.\n"
+printf "Minimo 6 caracteres\n"
+printf "[mysql pass]: "
+read KEYPASS_ASTERISK
+if [ -z "$KEYPASS_ASTERISK" ]
+then
+    echo "No valido"
+    exit 1
+fi
+
 
 # instalacion dependencias primarias
 yum -y install wget ca-certificates nano net-tools yum-utils
@@ -28,9 +83,16 @@ make
 ./contrib/scripts/get_mp3_source.sh
 make install
 make config
-service asterisk start
-service asterisk status
- 
+
+# configuracion de certificado
+cd contrib/scripts
+mkdir -p /etc/asterisk/keys
+./ast_tls_cert -C "$HOST_MACHINE" -O "$COMPANY" -d /etc/asterisk/keys
+
+# configuraciones estandar asterisk
+cd $DIR_SCRITP
+cp confs/* /etc/asterisk/
+
 # error: Unable to connect to remote asterisk (does /var/run/asterisk/asterisk.ctl exist?)
 # si obtienes este error se debe deshabilitar selinux usando la siguiente linea: 
 setenforce 0 && sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
@@ -38,9 +100,6 @@ setenforce 0 && sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 # desactivar firewalld (si es necesario)
 systemctl stop firewalld
 systemctl disable firewalld
-
-ROOTUSER=root
-KEYPASS=123456
 
 # instalacion web server
 yum -y install httpd
@@ -56,11 +115,43 @@ yum -y install mysql-community-server
 systemctl start mysqld
 systemctl enable mysqld
 
-mysql -u root -e "use mysql; 
+mysql -uroot -e "use mysql; 
                   UPDATE user SET password=PASSWORD('$KEYPASS') WHERE user='$ROOTUSER'; 
                   FLUSH PRIVILEGES;"
 
-cd $DIR_SCRITP
+mysql -uroot -p"$KEYPASS" -e "CREATE DATABASE asterisk;
+                              GRANT ALL PRIVILEGES ON asterisk.* TO 'asterisk'@'localhost' IDENTIFIED BY "$KEYPASS_ASTERISK"; 
+                              FLUSH PRIVILEGES;"
 
-cp confs/* /etc/asterisk/
+mysql -uroot -p"$KEYPASS" -e "CREATE TABLE asterisk.ast_cdr (
+                                calldate datetime NOT NULL default '0000-00-00 00:00:00', 
+                                clid varchar(80) NOT NULL default '', 
+                                src varchar(80) NOT NULL default '', 
+                                dst varchar(80) NOT NULL default '', 
+                                dcontext varchar(80) NOT NULL default '', 
+                                channel varchar(80) NOT NULL default '', 
+                                dstchannel varchar(80) NOT NULL default '', 
+                                lastapp varchar(80) NOT NULL default '', 
+                                lastdata varchar(80) NOT NULL default '', 
+                                duration int(11) NOT NULL default '0', 
+                                billsec int(11) NOT NULL default '0', 
+                                disposition varchar(45) NOT NULL default '', 
+                                amaflags int(11) NOT NULL default '0', 
+                                accountcode varchar(20) NOT NULL default '', 
+                                uniqueid varchar(32) NOT NULL default '', 
+                                userfield varchar(255) NOT NULL default '' 
+                              );"
+
+
+cd /etc/asterisk/
+sed -i "s/^externip=public_ip/externip=$IP_MACHINE/g" sip.conf
+sed -i "s/^localnet=local_ip/localnet=$IP_MACHINE/g" sip.conf
+sed -i "s/asterisk_db_password/$KEYPASS_ASTERISK/g" res_mysql.conf
+sed -i "s/asterisk_db_password/$KEYPASS_ASTERISK/g" cdr_mysql.conf
+
+
+
+systemctl start asterisk
+systemctl status asterisk
+
 
